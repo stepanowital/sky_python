cdfe1e4f149b5c1e8b91830c144c6f92e1bb84f5
stepanowital <stepanowital@gmail.com>
2022-09-08 23:16:35 +0600

    11.2 in study

---
+++ 11_lesson/11.2/main.py
@@ -0,0 +1,30 @@
+# from flask import Flask
+#
+#
+# app = Flask(__name__)
+#
+# @app.route("/")
+# def page_index():
+#
+# 	page_content = """
+#     <h1>Смотри, это — моя страничка</h1>
+#     <p>Страничек много, но эта – моя!</p>
+#     <p>Фронтенд – мой лучший друг!</p>
+#     """
+# 	return page_content
+#
+# app.run(debug=True)
+#
+#
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta charset="UTF-8">
+      <title>Заголовок документа</title>
+  </head>
+  <body>
+    <!— Контент находится здесь—>
+  </body>
+</html>
