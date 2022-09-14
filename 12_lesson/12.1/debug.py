cd441bc5ddb51a6a8c4ecad16fd49bde5d72048c
stepanowital <stepanowital@gmail.com>
2022-09-14 23:34:58 +0600

    add debug.py

---
+++ 12_lesson/12.1/debug.py
@@ -0,0 +1,81 @@
+print("строка 1")
+print("строка 2")
+print("строка 3")
+print("строка 4")
+print("строка 5")
+
+
+# def my_max(items):
+#   current_max = items[0]
+#
+#   for index in range(1, len(items)):
+#     item = items[index]
+#     if item > current_max:
+#       current_max = item
+#
+#   return current_max
+#
+# the_max = my_max([99, 1, 0, -99, -1, 105])
+# print(the_max)
+
+
+# a = True
+#
+# if a:
+# 	print("Значение Истина")
+# else:
+# 	print("Значение Ложь")
+#
+#
+# numbers = ["ноль", "один", "два", "три", "четыре", "пять"]
+# index = 5
+#
+# # измените значение index здесь
+#
+# if 0 <= index <= 5:
+# 	print(numbers[index])
+
+# a = 1
+# print(a)
+# a = 2
+# print(a)
+# a = 3
+# print(a)
+# a = 4
+# print(a)
+# a = 5
+# print(a)
+# a = 6
+# print(a)
+
+
+# a = 1
+# b = 2
+# c = 3
+#
+# a = 4
+# b = 5
+# c = 6
+#
+# del a
+# del b
+# del c
+
+
+# a = 2**2+2**2**2+2**2
+#
+# a *= 2
+#
+# a *= a
+#
+# print(a)
+
+
+
+line = "В аквариуме у Харитона четыре рака да три тритона."
+
+line.replace("р", "")
+
+count = line.count("р")
+
+a = 10
